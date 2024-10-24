e9a28555c5330313884b2920950804967ba2ab4e
tpwls9325 <tpwls9325@naver.com>
2024-10-24 22:21:12 +0900

    셀렉트 씬 수정

---
+++ Assets/PSJ/_Script/SelectScene/SelectPlayerState.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPlayerState : MonoBehaviour
+{
+    public MarioStatus status;
+    
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(status == MarioStatus.SuperMario)
+        {
+            animator.SetBool("IsSuper", true);
+        }
+        else if(status == MarioStatus.RaccoonMario) 
+        {
+            animator.SetBool("IsRaccoon", true);
+        }
+        else if(status == MarioStatus.FireMario)
+        {
+            animator.SetBool("IsFire", true);
+        }
+        else
+        {
+            animator.SetBool("IsNormal", true);
+        }
+    }
+}
---
+++ Assets/PSJ/_Script/SelectScene/SelectSceneManager.cs
@@ -0,0 +1,288 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectSceneManager : MonoBehaviour
+{
+    public GameObject map1Obj;
+    public GameObject map2Obj;
+    public GameObject map3Obj;
+    public GameObject map4Obj;
+    public GameObject mapBossObj;
+
+
+    public Transform StartPos;
+    private Transform map1Pos;
+    private Transform map2Pos;
+    private Transform map3Pos;
+    private Transform map4Pos;
+    private Transform mapBossPos;
+
+    public GameObject Player;
+
+
+    private float moveSpeed = 3f;
+
+    public int clearStage; //클리어 스테이지
+    public int currentStage = 0; //현재 스테이지
+    public bool onRoad; //길목
+    public bool itemStage;
+
+
+    void Start()
+    {
+        Player.transform.position = StartPos.position;
+        map1Pos = map1Obj.transform;
+        map2Pos = map2Obj.transform;
+        map3Pos = map3Obj.transform;
+        map4Pos = map4Obj.transform;
+        mapBossPos = mapBossObj.transform;
+
+        clearStage = 0;
+    }
+
+    void Update()
+    {
+        //Debug.Log("currentStage = " + currentStage);
+        //Debug.Log("OnRoad: " + onRoad);
+        switch (currentStage)
+        {
+            case 0:
+                if (Input.GetKeyDown(KeyCode.RightArrow) && !onRoad)
+                {
+                    Player.transform.position = new Vector2(map1Pos.position.x, StartPos.position.y);
+                    onRoad = true;
+                }
+                else if (Input.GetKeyDown(KeyCode.LeftArrow) && onRoad)
+                {
+                    Player.transform.position = StartPos.position;
+                    onRoad = false;
+                }
+                else if (Input.GetKeyDown(KeyCode.UpArrow) && onRoad)
+                {
+                    Player.transform.position = map1Pos.position;
+                    currentStage = 1;
+                    onRoad = false;
+                }
+                break;
+             case 1:
+                if (Input.GetKeyDown(KeyCode.DownArrow) && !onRoad)
+                {
+                    Player.transform.position = new Vector2(map1Pos.position.x, StartPos.position.y);
+                    currentStage = 0;
+                    onRoad = true;
+                }
+                else if(Input.GetKeyDown(KeyCode.Z) && !onRoad)
+                {
+                    Debug.Log("Map1-1");
+                    //SceneManager.CreateScene("Map1-1"); 씬전환
+                    //클리어했다면
+                    clearStage = 1;
+                    map1Obj.GetComponent<SpriteRenderer>().enabled = true;
+                }
+                if (clearStage >= 1)
+                {
+                    if(Input.GetKeyDown(KeyCode.RightArrow) && !onRoad)
+                    {
+                        Player.transform.position = new Vector2(mapBossPos.position.x, map1Pos.position.y);
+                        onRoad = true;
+                    }
+                    else if(Input.GetKeyDown(KeyCode.LeftArrow) && onRoad)
+                    {
+                        Player.transform.position = map1Pos.position;
+                        onRoad = false;
+                    }
+                    else if(Input.GetKeyDown(KeyCode.RightArrow) && onRoad)
+                    {
+                        Player.transform.position = map2Pos.position;
+                        currentStage = 2;
+                        onRoad = false;
+                    }
+                }
+                break;
+
+            case 2:
+                if(Input.GetKeyDown(KeyCode.LeftArrow) && !onRoad)
+                {
+                    Player.transform.position = new Vector2(mapBossPos.position.x, map1Pos.position.y);
+                    onRoad = true;
+                }
+                else if(Input.GetKeyDown(KeyCode.LeftArrow) && onRoad && !itemStage)
+                {
+                    Player.transform.position = map1Pos.position;
+                    currentStage = 1;
+                    onRoad = false;
+                }
+                else if(Input.GetKeyDown(KeyCode.Z) && !onRoad)
+                {
+                    Debug.Log("Map1-2");
+                    //SceneManager.CreateScene("Map1-2"); 씬전환
+                    //클리어했다면
+                    clearStage = 2;
+                    map2Obj.GetComponent<SpriteRenderer>().enabled = true;
+                }
+                if (clearStage >= 2)
+                {
+                    if(Input.GetKeyDown(KeyCode.RightArrow) && !onRoad)
+                    {
+                        Player.transform.position = map3Pos.position;
+                        currentStage = 3;
+                    }
+                    else if(Input.GetKeyDown(KeyCode.DownArrow) && !onRoad)
+                    {
+                        Player.transform.position = new Vector2(Player.transform.position.x, map4Pos.position.y);
+                        onRoad = true;
+                    }
+                    else if(Input.GetKeyDown(KeyCode.UpArrow) && onRoad && !itemStage)
+                    {
+                        Player.transform.position = map2Pos.position;
+                        currentStage = 2;
+                        onRoad = false;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.UpArrow) && onRoad && itemStage)
+                    {
+                        Player.transform.position = new Vector2(Player.transform.position.x, map4Pos.position.y);
+                        onRoad = true;
+                        itemStage = false;
+                    }
+                    else if(Input.GetKeyDown(KeyCode.RightArrow) && onRoad && !itemStage)
+                    {
+                        Player.transform.position = map4Pos.position;
+                        currentStage = 4;
+                        onRoad = false;
+                    }
+                    else if(Input.GetKeyDown(KeyCode.DownArrow) && onRoad && clearStage >= 4)
+                    {
+                        Player.transform.position = new Vector2(Player.transform.position.x, mapBossPos.position.y);
+                        onRoad = true;
+                        itemStage = true;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.LeftArrow) && onRoad && itemStage)
+                    {
+                        Player.transform.position = mapBossPos.position;
+                        currentStage = 5;
+                        onRoad = false;
+                    }
+
+                }
+                break;
+
+            case 3:
+                if (Input.GetKeyDown(KeyCode.LeftArrow) && !onRoad)
+                {
+                    Player.transform.position = map2Pos.position;
+                    currentStage = 2;
+                    onRoad = false;
+                }
+                else if (Input.GetKeyDown(KeyCode.Z) && !onRoad)
+                {
+                    Debug.Log("Map1-3");
+                    //SceneManager.CreateScene("Map1-3"); 씬전환
+                    //클리어했다면
+                    clearStage = 3;
+                    map3Obj.GetComponent<SpriteRenderer>().enabled = true;
+                }
+                break;
+
+            case 4:
+                if(Input.GetKeyDown(KeyCode.LeftArrow) && !onRoad)
+                {
+                    Player.transform.position = new Vector2(map2Pos.position.x, Player.transform.position.y);
+                    onRoad = true;
+                }
+                else if(Input.GetKeyDown(KeyCode.UpArrow) && onRoad && !itemStage)
+                {
+                    Player.transform.position = map2Pos.position;
+                    currentStage = 2;
+                    onRoad = false;
+                }
+                else if (Input.GetKeyDown(KeyCode.UpArrow) && onRoad && itemStage)
+                {
+                    Player.transform.position = new Vector2(Player.transform.position.x, map4Pos.position.y);
+                    onRoad = true;
+                    itemStage = false;
+                }
+                else if(Input.GetKeyDown(KeyCode.RightArrow) && onRoad && !itemStage)
+                {
+                    Player.transform.position = map4Pos.position;
+                    currentStage = 4;
+                    onRoad = false;
+                }
+                else if(Input.GetKeyDown(KeyCode.Z) && !onRoad && clearStage >= 3)
+                {
+                    Debug.Log("Map1-4");
+                    //SceneManager.CreateScene("Map1-4"); 씬전환
+                    //클리어했다면
+                    clearStage = 4;
+                    map4Obj.GetComponent<SpriteRenderer>().enabled = true;
+                }
+                if(clearStage >= 4)
+                {
+                    if(Input.GetKeyDown(KeyCode.DownArrow) && onRoad)
+                    {
+                        Player.transform.position = new Vector2(Player.transform.position.x, mapBossPos.position.y);
+                        itemStage = true;
+                        onRoad = true;
+                    }
+                    if(Input.GetKeyDown(KeyCode.LeftArrow) && onRoad && itemStage)
+                    {
+                        Player.transform.position = mapBossPos.position;
+                        currentStage = 5;
+                        itemStage = false;
+                        onRoad = false;
+                    }
+                }
+                break;
+
+            case 5:
+                if(Input.GetKeyDown(KeyCode.RightArrow) && !onRoad)
+                {
+                    Player.transform.position = new Vector2(map2Pos.position.x,Player.transform.position.y);
+                    onRoad = true;
+                    itemStage = true;
+                }
+                else if(Input.GetKeyDown(KeyCode.RightArrow) && onRoad && !itemStage)
+                {
+                    Player.transform.position = map4Pos.position;
+                    onRoad = false;
+                    currentStage = 4;
+                }
+                else if(Input.GetKeyDown(KeyCode.UpArrow) && onRoad && itemStage)
+                {
+                    Player.transform.position = new Vector2(Player.transform.position.x,map4Pos.position.y);
+                    onRoad = true;
+                    itemStage = false;
+                }
+                else if(Input.GetKeyDown(KeyCode.UpArrow) && onRoad && !itemStage)
+                {
+                    Player.transform.position = map2Pos.position;
+                    onRoad = false;
+                    currentStage = 2;
+                }
+                else if(Input.GetKeyDown(KeyCode.LeftArrow) && onRoad && itemStage)
+                {
+                    Player.transform.position = mapBossPos.position;
+                    onRoad = false;
+                    itemStage = false;
+                }
+                else if(Input.GetKeyDown(KeyCode.DownArrow) && onRoad && !itemStage)
+                {
+                    Player.transform.position = new Vector2(Player.transform.position.x, mapBossPos.position.y);
+                    onRoad = true;
+                    itemStage = true;
+                }
+                else if(Input.GetKeyDown(KeyCode.Z) && !onRoad && !itemStage)
+                {
+                    Debug.Log("MapBoss");
+                    //SceneManager.CreateScene("MapBoss"); 씬전환
+                    //클리어했다면
+                    clearStage = 5;
+                    mapBossObj.GetComponent<SpriteRenderer>().enabled = true;
+
+                }
+                break;
+        }        
+
+    }
+}
