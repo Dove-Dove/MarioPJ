e9189ded7ec7c381af9e6a5e320438d207323d5a
DESKTOP-ATAQR4T\Dove <99jinjw@naver.com>
2024-10-30 15:12:35 +0900

    파이프 작업중 및 맵 매니저 수정

--- Assets/J/JScripts/Pipe.cs
+++ Assets/J/JScripts/Pipe.cs
@@ -7,24 +7,25 @@ public class Pipe : MonoBehaviour
     public GameObject PipemMovement;
     public bool Move = false;
 
-    float pipeTime = 0; 
-    bool inPipe = false;
+    public float pipeTime = 0;
+    public bool inPipe = false;
     bool outPipe = false;
 
     void Start()
     {
-        
+        PipemMovement = GameObject.Find("Mario");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(inPipe)
-            pipeTime = Time.unscaledDeltaTime;
-
-   
-
-            
+        Move = PipemMovement.GetComponent<Player_Move>().isPipe;
+        if (inPipe)
+        {
+            Time.timeScale = 0;
+            pipeTime += Time.unscaledDeltaTime;
+        }
+          
     }
 
 
@@ -33,10 +34,27 @@ public class Pipe : MonoBehaviour
         if (collision.gameObject.tag == "Player" && Move )
         {
             inPipe = true;
-            if (pipeTime >= 3.0f)
-                collision.gameObject.transform.position = PipemMovement.transform.position;
-            
-            
+
+            //if (pipeTime >=3.0f )
+            //{
+            //    collision.gameObject.transform.position = PipemMovement.transform.position;
+            //    Time.timeScale = 1;
+            //    inPipe = false; 
+            //    pipeTime = 0;
+            //}
+
+            //collision.gameObject.transform.position = PipemMovement.transform.position;
+
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (pipeTime >= 3.0f)
+        {
+            Time.timeScale = 1;
+            collision.gameObject.transform.position = PipemMovement.transform.position;
+            inPipe = false;
+            pipeTime = 0;
         }
     }
 
